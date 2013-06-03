45c3fe5db930db8177c96973aeda47ed362ac26d
Abigail Thurmond <AbigailThurmond14@gmail.com>
2013-06-03 13:42:47 -0600

    GLKitViewController

---
+++ GLKitViewController.h
@@ -0,0 +1,13 @@
+//
+//  GLKitViewController.h
+//  Flashing Cube
+//
+//  Created by Abigail Thurmond on 5/31/13.
+//  Copyright (c) 2013 Abigail Thurmond. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@interface GLKitViewController : GLKViewController
+
+@end
