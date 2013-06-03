207771de918a63af114e9f9c59f44c5039b90596
Abigail Thurmond <AbigailThurmond14@gmail.com>
2013-06-03 13:40:56 -0600

    AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Flashing Cube
+//
+//  Created by Abigail Thurmond on 5/30/13.
+//  Copyright (c) 2013 Abigail Thurmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
